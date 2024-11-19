81755cd9e25f68fe3d99de1c4033b64182b425d6
Andre Luis <andreluisaraujo25@hotmail.com>
2024-11-19 17:40:20 -0300

    Acrescentado a função de inserir cerveja na tabela, alguns ajuste de indetação

--- backend/produtos_dao.py
+++ backend/produtos_dao.py
@@ -1,8 +1,6 @@
 from sql_connection import get_sql_connection
 
 def get_all_products(connection):
-    
-
     cursor = connection.cursor()
 
     query = "SELECT * FROM sistema_cervejaria.cerveja"
@@ -11,17 +9,38 @@ def get_all_products(connection):
 
     response = []
 
-    for(id_cerveja, nome, estoque, valor) in cursor:
+    for (id_cerveja, nome, estoque, valor) in cursor:
         response.append(
-                    {
-                        'id_cerveja' : id_cerveja, 
-                        'nome' : nome, 
-                        'estoque' : estoque, 
-                        'valor' : valor
-                    }
+            {
+                'id_cerveja': id_cerveja,
+                'nome': nome,
+                'estoque': estoque,
+                'valor': valor
+            }
         )
 
     return response
+
+
+def insert_new_product(connection, cerveja):
+    cursor = connection.cursor()
+    query = ("INSERT INTO cerveja "
+             "(nome, estoque, valor)"
+             "VALUES (%s, %s, %s)")
+
+    data = (cerveja['nome_cerveja'], cerveja['estoque'], cerveja['valor'])
+
+    cursor.execute(query, data)
+    connection.commit()
+    return cursor.lastrowid
+
+
 if __name__=='__main__':
     connection = get_sql_connection()
-    print(get_all_products(connection))
+    print(insert_new_product(connection, {
+       'nome_cerveja' : 'BUDWEISER',
+       'estoque' : '1',
+       'valor' : '13'
+   }))
+    
+    
